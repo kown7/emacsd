19884b9dc6be468d5e83315f25016172d9f2d16e
Kristoffer Nordstroem <kristoffer@nordstroem.ch>
2019-11-16 21:23:09 +0100

    ! make it run with borg instead of attic

--- bin/backup-borg.sh
+++ bin/backup-borg.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-REPOSITORY='/cygdrive/c/Users/kristoffer/Google Drive/docs.attic/'
+
+WINHOME=/mnt/c/Users/kristoffer/
+REPOSITORY="${WINHOME}/Google Drive/docs.attic/"
 
 # Backup all of /home and /var/www except a few
 # excluded directories
-attic create --stats                                \
-    "${REPOSITORY}"::agamemnon-`date +%Y-%m-%d`      \
-    /cygdrive/g/Documents                           \
-    --exclude /cygdrive/g/Documents/ETH/NuM\ 2010   \
+borg create --stats                                \
+    "${REPOSITORY}"::`hostname`-`date +%Y-%m-%d`   \
+    ${WINHOME}/Documents                           \
+    --exclude ${WINHOME}/Documents/ETH/NuM\ 2010   \
     --exclude '*.pyc'
 
 # Use the `prune` subcommand to maintain 7 daily, 4 weekly
 # and 6 monthly archives.
-attic prune -v "${REPOSITORY}" --keep-daily=7 --keep-weekly=4 --keep-monthly=6
+borg prune -v "${REPOSITORY}" --keep-daily=7 --keep-weekly=4 --keep-monthly=6
